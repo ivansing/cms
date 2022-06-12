bc5a3c0898428060ace3e3c7e012cef57f5e7e4f
ivansing <lancedealway@gmail.com>
2022-06-11 22:42:22 -0500

    autoload_classes

---
+++ core/Config.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Core;
+
+class Config {
+    private static $config = [
+        'version'            => '0.0.1',
+        'root_dir'           => '/cms_template', // This will likely be / on live server
+        'default_controller' => 'Cursos', // The default home controller
+        'default_layout'     => 'default', // Default layoiut that is used
+        'default_site_title' => 'Cursosalimentos', // Default Site title
+        'db_host'            => '127.0.01', // DB host please use IP address not domain
+        'db_name'            => 'cms_template', // DB name
+        'db_user'            => 'root', // DB user
+        'db_password'        => '', // DB password
+        'login_cookie_name'  => 'hradfafj8787' // Login cookie name
+    ];
+
+    public static function get($key) {
+        // If $key exist then pass to $config array else nothing
+        return array_key_exists($key, self::$config)? self::$config[$key] : NULL;
+    }
+}
--- index.php
+++ index.php
@@ -1,1 +1,32 @@
-<h2>Index</h2>
+<?php 
+ session_start();
+ // will grab namespace core
+ use \Core\Config;
+
+ // define constants
+ 
+ // This give us the exact path in the server
+ define('PROOT', __DIR__);
+
+ // Separator \ windows server and linux /
+ define('DS', DIRECTORY_SEPARATOR);
+
+ // Autoload classes in the array and build the full path
+ spl_autoload_register(function($className){
+    $parts = explode('\\', $className);
+    $class = end($parts);
+    array_pop($parts);
+    $path = strtolower(implode(DS, $parts));
+    $path = PROOT . DS . $path . DS . $class . '.php';
+    if(file_exists($path)) {
+        include($path);
+    }
+    
+ });
+
+ $dbName = Config::get('db_name');
+ var_dump($dbName);
+ 
+ 
+ // use for debug purposes
+ //var_dump(DS);
