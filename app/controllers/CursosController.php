75501057ff8a5f695e5bc84b1bc5f98630d1fcff
ivansing <lancedealway@gmail.com>
2022-06-22 08:15:20 -0500

    execute and query methods done

--- app/controllers/CursosController.php
+++ app/controllers/CursosController.php
@@ -8,12 +8,18 @@ class CursosController extends Controller {
     
     public function indexAction(){
         $db = DB::getInstance();
-       // Helpers::dnd($db, false); debug
+        $sql = "INSERT INTO cursos (`title`, `body`) VALUES (:title, :body)";
+        $bind = ['title' => 'nuevo curso', 'body' => 'cursos body'];
+        $query = $db->execute($sql, $bind);
+        $lastId = $query->lastInsertId();
+        Helpers::dnd($lastId);
+        /* $sql = "SELECT * FROM  cursos";
+        $query = $db->query($sql);
+        $cursos = $db->query($sql, )->getResults();
+        $count = $query->getLastInsertId();
+        Helpers::dnd($count);*/
         $this->view->setSiteTitle('Nuevos Cursos');
         $this->view->render();
     }
-
-    
-
     
 }
--- core/DB.php
+++ core/DB.php
@@ -8,8 +8,10 @@ use Core\{Config, Helpers};
 
 class DB {
     protected $_dbh, $_results, $_lastInsertId, $_rowCount = 0, $_fetchType = PDO::FETCH_OBJ, $_class, $_error = false;
+    protected $_stmt;
     protected static $_db;
 
+    // Database connection
     public function __construct() {
         $host = Config::get('db_host');
         $name = Config::get('db_name');
@@ -26,11 +28,46 @@ class DB {
             throw new Exception($e->getMessage());
         }
     }
-
+    // Singleton pattern for various  connection  instances
     public static function getInstance(){
         if(!self::$_db){
             self::$_db = new self();
         }
         return self::$_db;
+    }
+
+    public function execute($sql, $bind=[]){
+        $this->_results = null;
+        $this->_lastInsertId = null;
+        $this->_error = false;
+        $this->_stmt = $this->_dbh->prepare($sql);
+        if(!$this->_stmt->execute($bind)) {
+            $this->_error = true;
+        } else {
+            $this->_lastInsertId = $this->_dbh->lastInsertId();
+        }
+
+        return $this;
+    }
+
+    public function query($sql, $bind=[]) {
+        $this->execute($sql, $bind);
+        if(!$this->_error) {
+            $this->_rowCount = $this->_stmt->rowCount();
+            $this->_results = $this->_stmt->fetchAll($this->_fetchType);
+        }
+        return $this;
+    }
+
+    public function results(){
+        return $this->_results;
+    }
+
+    public function getCount() {
+        return $this->_rowCount;
+    }
+
+    public function lastInsertId(){
+        return $this->_lastInsertId;
     }
 }
